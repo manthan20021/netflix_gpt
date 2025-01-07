dd39035fcb550620c49eec84f208b286a3098765
Manthan harale <126886816+manthan20021@users.noreply.github.com>
2025-01-06 21:36:33 -0800

    add changes for responsiv mobail divais

--- src/component/CardContainer.jsx
+++ src/component/CardContainer.jsx
@@ -7,7 +7,7 @@ const CardContainer = ({mainMovis}) => {
     
    
   return (
-    <div className='h-auto -mt-32 flex flex-wrap bg-black pl-20' >
+    <div className='h-auto sm:-mt-32 -mt-[500px] grid grid-flow-row grid-cols-3 bg-black' >
         {
             mainMovis.map(movie => {
                 return  <Card  key={movie.id} cardMove={movie}/>
